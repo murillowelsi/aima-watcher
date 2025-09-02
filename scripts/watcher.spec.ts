1f791cc3cd94baab28ac6453e28402c970a7f305
Murillo Welsi <murillo.welsi@gmail.com>
2025-09-02 10:34:57 +0100

    created the project

--- scripts/watcher.spec.ts
+++ scripts/watcher.spec.ts
@@ -1,6 +1,6 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('/');
-  await page.pause();
+  await expect(page.getByRole('heading', { name: 'Bem vindo ao Portal de Renovações!' })).toBeVisible();
 });
